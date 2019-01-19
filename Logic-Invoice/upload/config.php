8dfea12f98fd9161ae453f4b15435ca3d7060e8d
Ubuntu <ubuntu@mail.rvrh.com>
2019-01-19 02:16:37 +0000

    use of logic invoice

---
+++ Logic-Invoice/upload/config.php
@@ -0,0 +1,12 @@
+<?php
+// URL
+define('APP_URL', 'https://www.rvrh.com/iv/');
+define('APP_SURL', 'https://www.rvrh.com/iv/');
+
+// Database
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'invoiceuser');
+define('DB_PASSWORD', '@1725');
+define('DB_DATABASE', 'logicInvoicedb');
+define('DB_PREFIX', 'cnmtl_');
